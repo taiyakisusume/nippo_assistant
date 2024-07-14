bb1e42ef19b798f2891bc6c2ce3534d6ee89458c
taiyakisusume <157872668+taiyakisusume@users.noreply.github.com>
2024-07-14 09:41:45 +0900

    feat: add target observer

--- src/content.ts
+++ src/content.ts
@@ -7,13 +7,50 @@ new MutationObserver(() => {
     if (oldUrl === currentUrl) return;
     oldUrl = currentUrl;
     window.dispatchEvent(new CustomEvent("urlChange"));
-}).observe(document.body, { // DOMの変化はurl更新の必要条件とみなす
+}).observe(document.body, {
+    // DOMの変化はurl更新の必要条件とみなす
     subtree: true,
     childList: true,
     attributes: true,
     characterData: true,
 });
+
+const targetSelector =
+    "body > div > main > div > div:nth-child(2) > table > tbody";
 window.addEventListener("urlChange", () => {
     if (location.pathname !== targetUrl.pathname) return;
-    console.log("content start");
+    const targetElement: Element | null =
+        document.querySelector(targetSelector);
+    if (targetElement == null) {
+        waitForTargetOccurrence.observe(document.body, {
+            subtree: true,
+            childList: true,
+            attributes: true,
+            characterData: true,
+        });
+        return;
+    }
+    registerObserver(targetElement);
 });
+const waitForTargetOccurrence = new MutationObserver(() => {
+    const targetElement: Element | null =
+        document.querySelector(targetSelector);
+    if (targetElement === null) return;
+    waitForTargetOccurrence.disconnect();
+    registerObserver(targetElement);
+});
+const registerObserver = (targetElement: Element) => {
+    targetCallback(targetElement)();
+    targetObserver(targetElement).observe(targetElement, {
+        subtree: true,
+        childList: true,
+        attributes: true,
+        characterData: true,
+    });
+};
+const targetObserver = (targetElement: Element) =>
+    new MutationObserver(targetCallback(targetElement));
+
+const targetCallback = (targetElement: Element) => () => {
+    console.log(targetElement.children.length);
+};
